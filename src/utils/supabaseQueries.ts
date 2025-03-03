f0ab245e909ecb1280adce151a11974ac1f39295
Alexander <aaalex2025@gmail.com>
2025-03-02 21:45:02 -0400

    fix: get project and task individual

--- src/utils/supabaseQueries.ts
+++ src/utils/supabaseQueries.ts
@@ -6,5 +6,21 @@ export const tasksQuery = supabase.from('tasks').select(`*, projects(id, name,sl
 export type ITasksQuery = QueryData<typeof tasksQuery>
 
 export const projectsQuery = supabase.from('projects').select()
-
 export type IProjectsQuery = QueryData<typeof projectsQuery>
+
+export const projectQuery = (slug: string) =>
+  supabase
+    .from('projects')
+    .select(
+      `*,
+  tasks (
+  id,
+  name,
+  status,
+  due_date
+  )
+  `,
+    )
+    .eq('slug', slug)
+    .single()
+export type IProjectQuery = QueryData<ReturnType<typeof projectQuery>>
