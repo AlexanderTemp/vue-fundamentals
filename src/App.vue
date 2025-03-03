8e31c101e55d9d0ec87b2865344712a10bb4014c
Alexander <aaalex2025@gmail.com>
2025-03-02 20:43:13 -0400

    feat(suspense): usage of suspense for async components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+/* Se usa suspense en este caso para las peticiones asíncronas que el componentes puede llegar a tener */
+</script>
 
 <template>
   <AuthLayout>
-    <RouterView />
+    <RouterView v-slot="{ Component, route }">
+      <Suspense v-if="Component" timeout="0">
+        <component :is="Component" :key="route.name"></component>
+        <template #fallback>
+          <span>Cargando...</span>
+        </template>
+      </Suspense>
+    </RouterView>
   </AuthLayout>
 </template>
